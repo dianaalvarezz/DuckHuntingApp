b00b8130373e681a812cd7a5880702939f14397d
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2025-02-01 19:39:36 -0600

    Create GameTimerTask.java

---
+++ GameTimerTask.java
@@ -0,0 +1,20 @@
+package edu.niu.android.duckhunting;
+import java.util.TimerTask;
+public class GameTimerTask extends TimerTask
+{
+    private Game game;
+    private GameView gameView;
+    public GameTimerTask(GameView view)
+    {
+        gameView = view;
+        game = view.getGame();
+        game.startDuckFromRightTopHalf();
+    }
+    public void run()
+    {
+        game.moveDuck();
+        if(game.duckOffScreen())
+            game.startDuckFromRightTopHalf();
+        gameView.postInvalidate();
+    }
+}
